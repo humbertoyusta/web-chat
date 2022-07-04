e17bea393910bea6c248b0d71928744fb686f8d3
humbertoyusta <humbertoyusta02@gmail.com>
2022-07-04 13:10:00 -0400

    Setting up postgres with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule, 
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+      port: 5434,
+      host: 'localhost',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
