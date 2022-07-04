a7ed1f4ea6b5c9d27e9fc5c419c6f4a5907093d8
humbertoyusta <humbertoyusta02@gmail.com>
2022-07-04 16:30:28 -0400

    using config service to set the database connection options

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,27 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
     UsersModule, 
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      database: 'postgres',
-      username: 'postgres',
-      password: 'postgres',
-      port: 5434,
-      host: 'localhost',
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return ({
+          type: 'postgres',
+          ...configService.get<object>('database'),
+          synchronize: true,
+          autoLoadEntities: true,
+        });
+      },
     }),
   ],
   controllers: [AppController],
---
+++ src/config/app.config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    database: {
+        type: process.env.DATABASE_TYPE,
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: process.env.DATABASE_PORT,
+        host: process.env.DATABASE_HOST,
+    },
+})
