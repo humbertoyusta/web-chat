7ffb9d8f7f42c38c3085ec903c880c43c2200f75
humbertoyusta <humbertoyusta02@gmail.com>
2022-07-06 16:34:58 -0400

    disabled registering two users with the same username

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,40 +2,26 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, HttpException, HttpStatus, Post, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
 import { SignInUserDto } from './dto/sign-in-user.dto';
 import { SignUpUserDto } from './dto/sign-up-user.dto';
 
 @Controller('api/auth')
 export class AuthController {
     constructor(
-        private readonly usersService: UsersService,
-        private readonly jwtService: JwtService,    
+        private readonly authService: AuthService,  
     ) {}
 
     @Post('sign-up')
     async signUp(@Body() user: SignUpUserDto): Promise<User> {
-        return await this.usersService.createUser(user);
+        return await this.authService.signUp(user);
     }
 
     @Get('sign-in')
     async signIn(@Body() user: SignInUserDto) {
-        const userInDb = await this.usersService.findOneByUsername(user.username);
-        if (!userInDb) {
-            throw new UnauthorizedException('Incorrect username or password');
-        }
-        else {
-            if (user.password === userInDb.password) {
-                return {
-                    access_token: this.jwtService.sign({user: user.username, password: user.password}),
-                };
-            }
-            else {
-                throw new UnauthorizedException('Incorrect username or password');
-            }
-        }
+        return await this.authService.signIn(user);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,22 +2,41 @@
 https://docs.nestjs.com/providers#services
 */
 
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { SignInUserDto } from './dto/sign-in-user.dto';
+import { SignUpUserDto } from './dto/sign-up-user.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+    ) { }
 
-    async validateUser(user: SignInUserDto) {
+    async signUp(user: SignUpUserDto) {
         const userInDb = await this.usersService.findOneByUsername(user.username);
-        if (userInDb && user.password == userInDb.password) {
-            const { password, ...finalUser } = userInDb;
-            return finalUser;
+        if (!userInDb) {
+            return this.usersService.createUser(user);
         }
         else {
-            return null;
+            throw new ConflictException('There is a registered user with that username');
+        }
+    }
+
+    async signIn(user: SignInUserDto) {
+        const userInDb = await this.usersService.findOneByUsername(user.username);
+        if (userInDb && user.password === userInDb.password) {
+            return {
+                access_token: this.jwtService.sign({
+                    user: user.username,
+                    password: user.password,
+                })
+            };
+        }
+        else {
+            throw new UnauthorizedException();
         }
     }
 }
