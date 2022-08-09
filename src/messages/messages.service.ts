8c2ef217f51434597c3f8e36b1e13170ee986232
humbertoyusta <humbertoyusta02@gmail.com>
2022-08-09 16:11:16 -0400

    sorting by date

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -4,6 +4,7 @@ https://docs.nestjs.com/providers#services
 
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Message } from './entities/message.entity';
 
@@ -12,22 +13,30 @@ export class MessagesService {
     constructor(
         @InjectRepository(Message)
         private readonly messagesRepository: Repository<Message>,
-    ) {}
+    ) { }
 
     async send(senderId: number, receiverId: number, text: string) {
-        const sender = {id: senderId};
-        const receiver = {id: receiverId};
-        return await this.messagesRepository.save({text, sender, receiver});
+        const sender = { id: senderId };
+        const receiver = { id: receiverId };
+        return await this.messagesRepository.save({ text, sender, receiver });
     }
 
-    async findAll(id: number, options?: {onlySent?: boolean, onlyReceived?: boolean}): Promise<Message[]> {
+    async findAll(id: number, options?: { onlySent?: boolean, onlyReceived?: boolean }): Promise<Message[]> {
+        let messagesList: Message[] = [];
         if (options && options.onlySent && options.onlyReceived)
-            return [];
+            return messagesList;
         else if (options && options.onlySent)
-            return await this.messagesRepository.find({where: {sender: {id}}, relations: ["receiver"]});
+            messagesList = await this.messagesRepository.find({ where: { sender: { id } }, relations: ["receiver"], order: {sentAt: 1}});
         else if (options && options.onlyReceived)
-            return await this.messagesRepository.find({where: {receiver: {id}}});
+            messagesList = await this.messagesRepository.find({ where: { receiver: { id } }, relations: ["receiver"] });
         else
-            return await this.messagesRepository.find({where: {sender: {id}} || {receiver: {id}}});
+            messagesList = await this.messagesRepository.find({ where: { sender: { id } } || { receiver: { id } }, relations: ["receiver"] });
+
+        for (const message of messagesList) {
+            const { passwordHash, ...receiverNoPass } = message.receiver;
+            message.receiver = receiverNoPass as User;
+        }
+
+        return messagesList;
     }
 }
